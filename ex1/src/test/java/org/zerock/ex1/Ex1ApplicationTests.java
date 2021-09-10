9f94b415699dd87df137d0a2923fd2c318b9f580
Joe <jhhong0930@gmail.com>
2021-09-10 10:15:13 +0900

    test 환경에서의 lombok 사용

--- ex1/src/test/java/org/zerock/ex1/Ex1ApplicationTests.java
+++ ex1/src/test/java/org/zerock/ex1/Ex1ApplicationTests.java
@@ -1,13 +1,16 @@
 package org.zerock.ex1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class Ex1ApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("contextLoads");
     }
 
 }
