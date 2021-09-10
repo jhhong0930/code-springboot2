59e2dc732adfea39a6d7a7c820d6aa7eb8b9d9aa
Joe <jhhong0930@gmail.com>
2021-09-10 13:00:46 +0900

    페이징 처리

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.ex2.entity.Memo;
 
 import java.util.Optional;
@@ -68,4 +72,51 @@ class MemoRepositoryTest {
 
         memoRepository.deleteById(mno);
     }
+
+    @Test
+    @DisplayName("페이징 처리 테스트")
+    void testPageDefault() {
+
+        // 1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
+        System.out.println("=======================================");
+
+        // 총 페이지 개수
+        System.out.println("Total Pages: " + result.getTotalPages());
+        // 전체 게시글 개수
+        System.out.println("Total Count: " + result.getTotalElements());
+        // 현재 페이지 번호 0부터 시작
+        System.out.println("Page Number: " + result.getNumber());
+        // 페이지당 데이터 개수
+        System.out.println("Page Size: " + result.getSize());
+        // 다음 페이지 존재 여부
+        System.out.println("has next page?: " + result.hasNext());
+        // 시작 페이지(0) 여부
+        System.out.println("first page?: " + result.isFirst());
+
+        System.out.println("=======================================");
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    @DisplayName("정렬 조건 테스트")
+    void testSort() {
+
+        Sort sort1 = Sort.by("mno").descending();
+
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(System.out::println);
+    }
+
 }
