fa0c13879317195c5a491b193f2b35bec21c8bb7
Joe <jhhong0930@gmail.com>
2021-09-10 23:50:56 +0900

    목록 처리

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -2,17 +2,24 @@ package org.zerock.guestbook.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.function.Function;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService{
 
-    private final GuestbookRepository guestbookRepository;
+    private final GuestbookRepository repository;
 
     @Override
     public Long register(GuestbookDTO dto) {
@@ -24,8 +31,20 @@ public class GuestbookServiceImpl implements GuestbookService{
 
         log.info("entity = {}", entity);
 
-        guestbookRepository.save(entity);
+        repository.save(entity);
 
         return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
     }
 }
--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTest.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTest.java
@@ -5,14 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 @SpringBootTest
 class GuestbookServiceTest {
 
     @Autowired
-    private GuestbookService guestbookService;
+    private GuestbookService service;
 
     @Test
     @DisplayName("DTO 객체를 엔티티 객체로 변환 테스트")
@@ -24,7 +25,33 @@ class GuestbookServiceTest {
                 .writer("user0")
                 .build();
 
-        System.out.println(guestbookService.register(guestbookDTO));
+        System.out.println(service.register(guestbookDTO));
     }
+
+    @Test
+    @DisplayName("목록 처리 테스트")
+    void testList() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("====================================");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("====================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+
+
 
 }
