93ddee33e464c3a83ead47a8fcaccd31a5c43942
Joe <jhhong0930@gmail.com>
2021-09-10 13:55:42 +0900

    쿼리 메서드와 @Query

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
@@ -8,8 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -117,6 +120,40 @@ class MemoRepositoryTest {
         Page<Memo> result = memoRepository.findAll(pageable);
 
         result.get().forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("쿼리 메서드 테스트")
+    void testQueryMethods() {
+
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+
+        for (Memo memo : list) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    @DisplayName("쿼리 메서드와 Pageable의 결합 테스트")
+    void testQueryMethodWithPageable() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+
+        result.get().forEach(System.out::println);
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    @DisplayName("쿼리 메서드 삭제 테스트")
+    void testDeleteQueryMethods() {
+        /*
+        - deleteBy는 하나씩 삭제를 처리하기 때문에 성능이 떨어진다
+        - deleteBy보다는 @Query를 이용한다
+         */
+        memoRepository.deleteMemoByMnoLessThan(10L);
     }
 
 }
