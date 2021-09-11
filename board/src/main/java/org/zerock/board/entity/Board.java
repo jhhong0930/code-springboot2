de253d08738a6c6f6502cf0c962a298bf7477f23
Joe <jhhong0930@gmail.com>
2021-09-11 14:50:03 +0900

    엔티티 클래스, repository 추가

---
+++ board/src/main/java/org/zerock/board/entity/Board.java
@@ -0,0 +1,25 @@
+package org.zerock.board.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "writer") // 연관관계 매핑 대상은 ToString 에서 제외
+public class Board extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    private Member writer;
+
+}
