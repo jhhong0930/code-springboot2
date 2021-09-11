c834c4557bc95be50eff8eeab0545a09391e4479
Joe <jhhong0930@gmail.com>
2021-09-11 15:53:19 +0900

    연관관계 테스트 - JPQL과 left (outer) join

--- board/src/main/java/org/zerock/board/repository/BoardRepository.java
+++ board/src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -1,7 +1,39 @@
 package org.zerock.board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Board;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    // 한개의 행(Object) 내에 Object[] 로 나온다
+    // 엔티티 클래스 내부에 연관관계가 있는 경우에는 join 뒤 on을 사용하지 않는다
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
+
+    // 연관관계가 없는 엔티티 조인 처리시에는 on 사용
+    @Query("select b, r from Board b left join Reply r on r.board = b where b.bno =:bno")
+    List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    // 목록 화면에 필요한 JPQL(게시물 번호/제목/작성시간, 회원 이름/이메일, 댓글 수)
+    @Query(value = "select b, w, count(r) " +
+            "from Board b " +
+            "left join b.writer w " +
+            "left join Reply r on r.board = b " +
+            "group by b",
+            countQuery = "select count(b) from Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);
+
+    // 조회 화면에서 필요한 JPQL
+    @Query("select b, w, count(r) " +
+            "from Board b left join b.writer w " +
+            "left outer join Reply r on r.board = b " +
+            "where b.bno =:bno")
+    Object getBoardByBno(@Param("bno") Long bno);
+
 }
--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTest.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -47,6 +53,56 @@ class BoardRepositoryTest {
 
         System.out.println(board);
         System.out.println(board.getWriter());
+    }
+
+    @Test
+    @DisplayName("연관관계가 있는 join 테스트")
+    void testGetBoardWithWriter() {
+
+        Object result = repository.getBoardWithWriter(100L);
+
+        Object[] arr = (Object[]) result;
+
+        System.out.println("================================");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    @DisplayName("연관관계가 없는 join 테스트")
+    void testGetBoardWithReply() {
+
+        List<Object[]> result = repository.getBoardWithReply(100L);
+
+        for (Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    @Test
+    @DisplayName("목록 화면에 필요한 JPQL 테스트")
+    void testGetBoardWithReplyCount() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = repository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+
+            Object[] arr = (Object[]) row;
+
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    @DisplayName("조회 화면에 필요한 JPQL 테스트")
+    void testGetBoardByBno() {
+
+        Object result = repository.getBoardByBno(100L);
+
+        Object[] arr = (Object[]) result;
+
+        System.out.println(Arrays.toString(arr));
     }
 
 }
