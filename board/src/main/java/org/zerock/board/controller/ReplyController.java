d94c81a0da6b5752d8bc5891460de83f00fc9cc8
Joe <jhhong0930@gmail.com>
2021-09-11 21:22:26 +0900

    JSON과 Ajax로 댓글 처리 - 조회 화면 처리

--- board/src/main/java/org/zerock/board/controller/ReplyController.java
+++ board/src/main/java/org/zerock/board/controller/ReplyController.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.board.dto.ReplyDTO;
 import org.zerock.board.service.ReplyService;
 
@@ -25,9 +22,39 @@ public class ReplyController {
     @GetMapping(value = "/board/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno) {
 
-        log.info("bno={}", bno);
+        log.info("bno = {}", bno);
 
         return new ResponseEntity<>(service.getList(bno), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO) {
+
+        log.info("replyDTO={}", replyDTO);
+
+        Long rno = service.register(replyDTO);
+
+        return new ResponseEntity<>(rno, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+
+        log.info("rno = {}", rno);
+
+        service.remove(rno);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO) {
+
+        log.info("replyDTO = {}", replyDTO);
+
+        service.modify(replyDTO);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
 }
