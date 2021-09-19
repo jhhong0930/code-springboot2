f616053185b2a5eb5934a389e9cd6e0b414af0d4
jhhong0930 <jhhong0930@gmail.com>
2021-09-19 17:51:23 +0900

    create project mreview

---
+++ mreview/src/main/java/com/example/mreview/MreviewApplication.java
@@ -0,0 +1,14 @@
+package com.example.mreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class MreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MreviewApplication.class, args);
+    }
+
+}
